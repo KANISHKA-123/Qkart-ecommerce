3ac5e7c9abc68aae8c1117b8af3ca45c7cec0564
KANISHKA-123 <kanishkam314@gmail.com>
2024-03-11 17:42:03 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://w3layouts.com/wp-content/uploads/2021/12/Shoppy-Kart-Website-Template-eCommerce-600x375.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
+
